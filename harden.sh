1f052282a95bf29a7ee4053db40b1e5013be77c3
Ubuntu <ubuntu@ip-172-31-30-81.us-east-2.compute.internal>
2019-02-13 01:03:54 +0000

    basic hardening script

---
+++ harden.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# all of this is cribbed from https://www.codelitt.com/blog/my-first-10-minutes-on-a-server-primer-for-securing-ubuntu/
+
+# knock knock housekeeping
+sudo apt update -y
+sudo apt upgrade -y
+
+# install a firewall and do some basic smart things
+sudo sed -i s/IPV6=no/IPV6=yes/g /etc/default/ufw
+sudo apt install ufw
+sudo ufw allow ssh
+sudo ufw allow http
+sudo ufw allow https
+#sudo ufw allow 53 # i believe this is necessary for AWS
+#sudo ufw allow 68 # i believe this is also necessary for AWS
+
+# install fail2ban and just let it do its thing
+sudo apt install fail2ban -y
+
+# restart the firewall (will temporarily disable ssh, so logging out now)
+sudo ufw disable && sudo ufw --force enable
